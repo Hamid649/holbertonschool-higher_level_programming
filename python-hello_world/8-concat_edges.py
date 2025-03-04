35e2d99a6e77a346d41b8938ae0c9095963a69ad
Hamid649 <gamid.mahmudov.gm@gmail.com>
2025-03-04 18:43:16 +0400

    8-concat_edges.py

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str1 = "object-oriented"
+str2 = "programming"
+str3 = "with"
+str4 = "Python"
+str = str1 + " " + str2 + " " + str3 + " " + str4
+print(str)
